131c902b44ee930d911bf2f7237cf37bc1ebab26
sunny-yo <dmsfh3@gmail.com>
2022-01-20 21:43:37 +0900

    220120

--- KimSunju/ch9_stack_queue/LEET_316_중복문자제거.py
+++ KimSunju/ch9_stack_queue/LEET_316_중복문자제거.py
@@ -1,24 +1,24 @@
-# class Solution:
-#     def removeDuplicateLetters(self, s: str) -> str:
-#         # 집합으로 정렬
-#         for char in sorted(set(s)):
-#             suffix = s[s.index(char):]
-#             # 전체 집합과 접미사 집합이 일치할때 분리 진행
-#             if set(s) == set(suffix):
-#                 return char + self.removeDuplicateLetters(suffix.replace(char, ''))
-#         return ''
-
 import collections
 
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        last_index = {c:i for i, c in enumerate(s)}
-        result = []
+        counter, seen, stack = collections.Counter(s), set(), []
 
-        for i, c in enumerate(s):
-            if c not in result and i < last_index[]
+        for char in s:
+            counter[char] -= 1
+            if char in seen:
+                continue
+            # 뒤에 붙일 문자가 남아 있다면 스택에서 제거
+            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+            stack.append(char)
+            seen.add(char)
 
-        return ''.join(result)
+        return ''.join(stack)
+
 
 s = Solution()
+print(s.removeDuplicateLetters("bcabc"))
 print(s.removeDuplicateLetters("cbacdcbc"))
+print(s.removeDuplicateLetters("cdadabcc"))
+
